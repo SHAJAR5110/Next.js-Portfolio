ac662cedcc0a1ebb9c86a036cb7954f2bddb57fb
SHAJAR5110 <shajarabbas602@gmail.com>
2025-03-05 02:13:47 +0500

    changes

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -63,12 +63,12 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a MERN Stack developer with a passion for creating
+            I am a MERN Stack and Agentic Ai developer with a passion for creating
             interactive and responsive web applications. I have experience
             working with JavaScript, React, Node.js, Next.js, MongoDB,
             HTML, CSS, and Git. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
+            looking to expand my knowledge and skill set. I am learning the Agentic Ai to get
+            better at building intelligent systems and enhancing my development capabilities.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -26,6 +26,8 @@ const HeroSection = () => {
                 1000,
                 "Web Developer",
                 1000,
+                "Agency Ai Developer",
+                1000,
                 "Programmer",
                 1000,
               ]}
